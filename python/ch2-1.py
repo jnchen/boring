9b0b811f2e313fee71186521ad1bc557a51b4059
jnchen <caojingchen@live.com>
2015-08-29 13:12:11 +0800

    update the function to encrypt

--- python/ch2-1.py
+++ python/ch2-1.py
@@ -3,5 +3,10 @@ import string
 def encrypt(question):
     table = string.maketrans('abcdefghijklmnopqrstuvwxyz','cdefghijklmnopqrstuvwxyzab')
     return string.translate(question,table)
+
+def encrypt2(question):
+    table  = string.maketrans(string.ascii_lowercase,string.ascii_lowercase[2:]+string.ascii_lowercase[:2])
+    return string.translate(question,table)
+
 question = raw_input("please input the stirng:")
-print encrypt(question)
+print encrypt2(question)
